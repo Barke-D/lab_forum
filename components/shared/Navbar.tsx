cdf04ad02a6ddbbb79d9df774e7d033183184e05
Barkot <barkoklord@gmail.com>
2024-05-27 16:35:29 +0300

    first commit

---
+++ components/shared/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full fixed flex items-center justify-between shadow-xl py-3 bg-white">
+      <div>
+        <h1 className="font-bold text-2xl tracking-wider md:px-6">
+          DigitalLab
+        </h1>
+      </div>
+      <div></div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ lib/mongoose.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+let isConnected: boolean = false;
+
+export const connectToDatabase = async () => {
+  mongoose.set("strictQuery", true);
+
+  if (!process.env.MONGODB_URL) {
+    return console.log("MISSING MONGODB_URL");
+  }
+
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      dbName: "chat_system",
+      bufferCommands: true,
+      connectTimeoutMS: 10000,
+    });
+
+    isConnected = true;
+
+    console.log("MongoDB is connected");
+  } catch (error) {
+    console.log("MongoDB connection failed", error);
+  }
+};
